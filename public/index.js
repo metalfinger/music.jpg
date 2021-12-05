5a6175160034d2af473924c668639a6814b4c4fa
Hiren <hir.012612@gmail.com>
2021-12-06 03:00:49 +0530

    fixed typo

--- public/index.js
+++ public/index.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     function fetchAllData() {
 
         console.log("Fetching data from database");
-        c
+
 
         fetch('getall', {
                 method: 'POST',
